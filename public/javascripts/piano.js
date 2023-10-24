09563c1863f5d7782ee53701f0f12862101d5069
leemingoo <dwmapi@naver.com>
2023-10-24 18:08:11 +0900

    feat: 마우스 클릭 이벤트 받아 mp3 재생

--- public/javascripts/piano.js
+++ public/javascripts/piano.js
@@ -1,14 +1,27 @@
 const keys = document.querySelectorAll('.key');
-const audio = document.querySelectorAll('audio');
+const audio = document.getElementById('audio');
+const sustainPedal = document.getElementById('sustain-pedal');
+let isSustainOn = false;
 
 keys.forEach(key => {
-  key.addEventListener('click', () => {
+  key.addEventListener('mousedown', () => {
     const note = key.getAttribute('data-note');
-    audio.item.play();
-
-    key.style.backgroundColor = 'red';
-    setTimeout(() => {
-      key.style.backgroundColor = key.classList.contains('white') ? 'white' : 'black';
-    }, 100);
+    audio.src = `./sounds/${note}.mp3`;
+    audio.play();
   });
+
+  key.addEventListener('mouseup', () => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+
+  key.addEventListener('mouseout', () => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+
+  key.style.backgroundColor = 'green';
+  setTimeout(() => {
+    key.style.backgroundColor = key.classList.contains('white') ? 'white' : 'black';
+  }, 100);
 });
