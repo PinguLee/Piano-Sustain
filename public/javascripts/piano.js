90964180101ad292f468ca529c7de52555d559cb
leemingoo <dwmapi@naver.com>
2023-10-24 17:04:46 +0900

    feat: 클릭 시 빨간색으로 칠해 클릭 구분

--- public/javascripts/piano.js
+++ public/javascripts/piano.js
@@ -1,10 +1,14 @@
 const keys = document.querySelectorAll('.key');
-const audio = document.getElementById('audio');
+const audio = document.querySelectorAll('audio');
 
 keys.forEach(key => {
-    key.addEventListener('click', () => {
-        const note = key.getAttribute('data-note');
-        audio.src = `sounds/${note}.mp3`; // 소리 파일의 경로를 설정하세요.
-        audio.play();
-    });
+  key.addEventListener('click', () => {
+    const note = key.getAttribute('data-note');
+    audio.item.play();
+
+    key.style.backgroundColor = 'red';
+    setTimeout(() => {
+      key.style.backgroundColor = key.classList.contains('white') ? 'white' : 'black';
+    }, 100);
+  });
 });
